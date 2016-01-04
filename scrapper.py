1f91d285ba23bd0e840dd262582b347cbbeebcac
Rudolf Copi Eckelberg <rudolfce@gmail.com>
2016-01-04 21:34:38 -0200

    Added query form

--- scrapper.py
+++ scrapper.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 import miner
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<user_name>')
-def index(user_name=None):
-    if user_name:
-        page_output = "user"
-        user = miner.mine_user(user_name)
-    else:
-        page_output = "index"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        page_output="index"
         user = None
+        return render_template("index.html", user=user, page_output=page_output)
+    else:
+        return redirect(request.form['user_name'])
+
+
+@app.route('/<user_name>')
+def show_user(user_name=None):
+    page_output = "user"
+    user = miner.mine_user(user_name)
     return render_template("index.html", user=user, page_output=page_output)
 
 if __name__ == "__main__":
